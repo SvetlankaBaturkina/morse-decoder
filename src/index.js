25428230a87c7bfcd1f6e79243a1b9aa5e401d60
SvetlankaBaturkina <melkaya2025sveta@gmail.com>
2022-02-25 03:06:32 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -34,13 +34,33 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    ' ': ' '
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
+    let resultString = '';
+    let arrString = [];
+    for(let i = 0; i < expr.length; i += 10) { // выделим подстоки по 10 символов 
+        arrString.push(expr.substr(i, 10)); // запишем в созданный массив
+    };
+    arrString.forEach((item) => { // для каждого элемента массива
+        let codeString = ''; // переведем каждый элемент массива в строку
+        if (item === '**********') { // найдем закодированный пробел
+            codeString += ' ';
+        };
+            for (let i = 0; i < item.length; i += 2) {
+                if (item.charAt(i) + item.charAt(i + 1) === '11') { 
+                    codeString += '-'; // найдем в коде тире
+                };
+                if (item.charAt(i) + item.charAt(i + 1) === '10') { 
+                    codeString += '.'; // найдем в коде точки
+                };
+            };
+        resultString += MORSE_TABLE[codeString]; // раскодируем строку
+    });
+    return resultString; // возвращаем результат
+};
 module.exports = {
     decode
-}
+};
